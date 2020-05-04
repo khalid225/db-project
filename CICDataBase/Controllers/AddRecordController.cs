efe1f7aeab6b3548550ecee2fe4601f9f84b3490
@github225 <msart4real@yahoo.com>
2020-05-04 01:04:28 +0100

    Initial commit with a bad UI and many repetitive codes

---
+++ CICDataBase/Controllers/AddRecordController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CICDataBase.Controllers
+{
+    public class AddRecordController : Controller
+    {
+        // GET: AddRecord
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add(CICDataBase.Models.Record ModelRecord)
+        {
+            return View();
+        }
+    }
+}
---
+++ CICDataBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CICDataBase.MvcApplication" Language="C#" %>
